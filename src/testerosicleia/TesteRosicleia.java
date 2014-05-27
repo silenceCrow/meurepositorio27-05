c9555718d60869e471338b9e91377118d1b1fbcc
bruno_fortunato <bruno_fortunato@10.1.48.205>
2014-05-27 15:32:52 -0300

    Inicialização de um projeto

---
+++ src/testerosicleia/TesteRosicleia.java
@@ -0,0 +1,15 @@
+
+package testerosicleia;
+
+import javax.swing.JOptionPane;
+
+
+public class TesteRosicleia {
+
+   
+    public static void main(String[] args) {
+       
+        
+        JOptionPane.showMessageDialog(null,"FMS_FML");
+    }
+}
